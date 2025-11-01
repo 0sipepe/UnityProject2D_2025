6b6f51bc81cad112b73b84203b0b27c03c61074d
0sipepe <osipenko270104@mail.ru>
2025-11-01 16:19:47 +0300

    правки самойлова

--- Assets/Scripts/Moving.cs
+++ Assets/Scripts/Moving.cs
@@ -15,12 +15,14 @@ public class Moving : MonoBehaviour
     private float jumpHeight;
 
     [SerializeField]
+
     // Не понятное название. Это лимит по расстоянию, или по времени?
-    private float limitToStartNewJump;
+    private float reserveDistance = 3;
 
     private bool isJumpingUp = false;
-    private bool isJumpingDown = false;
+    private bool isJumpingDown = true;
     private bool hasReachedTheChange = false;
+    private bool isGrounded;
     private float startPos;
     private bool isJumpReserved = false;
 
@@ -34,33 +36,100 @@ public class Moving : MonoBehaviour
         _rigidbody = GetComponent<Rigidbody2D>();
         gameInput.OnJumpAction += GameInput_OnJumpAction;
 
-        _updateJumpingState = NotJumping;
+        //_updateJumpingState = NotJumping;
+
+
     }
 
     private void GameInput_OnJumpAction(object sender, System.EventArgs e)
     {
-        //Debug.Log("distance before ground = " + IsNearGroundRaycast());
-        if (!isJumpReserved && DistanceBeforeGroundRaycast() <= 0.4)
+
+
+        if (!isJumpReserved && (IsAvailableToReserveAJump() && (isJumpingDown || isGrounded)))
         {
             isJumpReserved = true;
-            
-           //Debug.Log("button pushed && reserved a jump");
+
+            Debug.Log("reserved");
         }
     }
 
-    private Func<Action> _updateJumpingState;  
+    //private Func<Action> _updateJumpingState;
     private void Update()
     {
         Vector2 input = gameInput.GetMovementVectorNormalized();
+        if (isJumpReserved && isGrounded)
+        {
+            isJumpingUp = true;
+            isJumpingDown = false;
+            _rigidbody.AddForceY(jumpHeight, ForceMode2D.Impulse);
+            isJumpReserved = false;
+        }
+        if (isJumpingUp)
+        {
 
-        _updateJumpingState = () => _updateJumpingState();
+            float distancePassed = DistanceBeforeGroundRaycast();
+            float distanceBeforeChangingDirectionOfJump = jumpHeight - distancePassed;
+
+            Debug.Log("is jumping");
+            hasReachedTheChange = distanceBeforeChangingDirectionOfJump <= reserveDistance;
+            if (hasReachedTheChange)
+            {
+                isJumpingUp = false;
+                isJumpingDown = true;
+
+            }
+
+        }
+        if (isJumpingDown)
+        {
+            if (isGrounded)
+            {
+                isJumpingDown = false;
+            }
+
+        }
+
+
+        //_updateJumpingState = () => _updateJumpingState();
 
         _rigidbody.linearVelocityX = input.x * moveSpeed;
     }
 
-   
-    
-    // Плохое назнавние: судя по нему, метод должен возвращать bool
+
+
+
+
+
+
+    //возвращает, можно ли зарезервировать прижок
+    private bool IsAvailableToReserveAJump()
+    {
+        bool isNearGround = false;
+        Vector3 player = transform.position;
+        player.y = transform.position.y - _collider.size.y / 2f;
+
+        Debug.DrawRay(player, Vector2.down * reserveDistance, Color.blue);
+
+        RaycastHit2D hit = Physics2D.Raycast(player, Vector2.down, reserveDistance, LayerMask.NameToLayer("Floor"));
+        if (hit.collider != null)
+        {
+            if (hit.distance <= reserveDistance)
+            {
+                isNearGround = true;
+                Debug.Log("is available");
+            }
+        }
+        else
+        {
+            isNearGround = false;
+            Debug.Log("is not available");
+        }
+        Debug.Log("inside method" + isNearGround);
+        return isNearGround;
+    }
+
+    //метод возвращает расстояние перед землей
+    //разделить на 2 метода - бул из граундед и флоат дистансе
     private float DistanceBeforeGroundRaycast()
     {
         float distanceBeforeGround;
@@ -73,9 +142,10 @@ public class Moving : MonoBehaviour
         Debug.DrawRay(player, down, Color.blue);
 
         RaycastHit2D hit = Physics2D.Raycast(player, down);
-        if (hit.collider != null && hit.collider.gameObject.layer == LayerMask.NameToLayer("Floor")) 
+        if (hit.collider != null && hit.collider.gameObject.layer == LayerMask.NameToLayer("Floor"))
         {
-            distanceBeforeGround = hit.distance;            
+            distanceBeforeGround = hit.distance;
+            Debug.Log(distanceBeforeGround);
         }
         else
         {
@@ -83,50 +153,77 @@ public class Moving : MonoBehaviour
         }
         return distanceBeforeGround;
     }
-    
 
 
-    private Action NotJumping()
+    //возвращает, столкнулись ли мы с полом
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (!(isJumpReserved && !isJumpingDown))
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Floor"))
         {
-            return NotJumping();
-        }
-        
-        isJumpingUp = true;
-        _rigidbody.AddForceY(jumpHeight);
-        isJumpReserved = false;
-
-        return JumpingUp();
-    }
-
-    private Action JumpingUp()
-    {
-        if (!hasReachedTheChange)
-        {
-            float distancePassed = transform.position.y - startPos;
-            float distanceBeforeChangingDirectionOfJump = jumpHeight - distancePassed;
-
-            hasReachedTheChange = distanceBeforeChangingDirectionOfJump <= limitToStartNewJump;
-            return JumpingUp();
+            Debug.Log("grounded");
+            isGrounded = true;
         }
 
-        //Debug.Log("Has reached the Change");
-        isJumpingUp = false;
-        isJumpingDown = true;
-        return JumpingDown();
     }
 
-    private Action JumpingDown()
+
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        // Debug.Log("distance before ground jumping down = " + IsNearGroundRaycast());
-        if (DistanceBeforeGroundRaycast() > 0.001f)
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Floor"))
         {
-            return JumpingDown();
-        }
+            Debug.Log("grounded");
+            isGrounded = true;
+            //if (isJumpReserved)
+            //    Jump()
 
-        isJumpingDown = false;
-        return NotJumping();
+        }
     }
+
+
+
+    //недоделанная стейт-машина
+
+    //    private Action NotJumping()
+    //    {
+    //        if (!(isJumpReserved && !isJumpingDown))
+    //        {
+    //            return NotJumping();
+    //        }
+
+    //        isJumpingUp = true;
+    //        _rigidbody.AddForceY(jumpHeight);
+    //        isJumpReserved = false;
+
+    //        return JumpingUp();
+    //    }
+
+    //    private Action JumpingUp()
+    //    {
+    //        if (!hasReachedTheChange)
+    //        {
+    //            float distancePassed = transform.position.y - startPos;
+    //            float distanceBeforeChangingDirectionOfJump = jumpHeight - distancePassed;
+
+    //            hasReachedTheChange = distanceBeforeChangingDirectionOfJump <= limitToStartNewJump;
+    //            return JumpingUp();
+    //        }
+
+    //        //Debug.Log("Has reached the Change");
+    //        isJumpingUp = false;
+    //        isJumpingDown = true;
+    //        return JumpingDown();
+    //    }
+
+    //    private Action JumpingDown()
+    //    {
+    //        // Debug.Log("distance before ground jumping down = " + IsNearGroundRaycast());
+    //        if (DistanceBeforeGroundRaycast() > 0.001f)
+    //        {
+    //            return JumpingDown();
+    //        }
+
+    //        isJumpingDown = false;
+    //        return NotJumping();
+    //    }
 
 }
